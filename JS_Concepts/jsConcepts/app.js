ade2ea9e248b2b3f711a0e7512548822b3670191
Sonu Kumar <kumarsonuk430@gmail.com>
2024-09-24 22:57:42 +0530

    js

---
+++ JS_Concepts/jsConcepts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// let hasDriverLicense = false;
+
+// const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+// if (hasDriverLicense) console.log("i can drive");
---
+++ JS_Concepts/jsConcepts/index.js
@@ -0,0 +1,44 @@
+// type Coversion
+
+const inputYear = "1912";
+// console.log(Number(inputYear));
+const num = "23";
+// console.log(String(num));
+
+// type coerecion
+
+// console.log("I am " + 23 + " Years old");
+// console.log("23" - 3 - 10);
+// console.log("23" / "2");
+
+// ***********************************************************
+//  Truthy and Falsy Values
+// 5 falsy values: 0, "",undefined,"null",NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(""));
+// console.log(Boolean("sonu"));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+// const money = 0;
+// if (money) {
+//   console.log("Dont't spaend ia all");
+// } else {
+//   console.log("you should get a job");
+// }
+
+// let height ;
+// if (height) {
+//   console.log("YAY! Heaight is defined");
+// } else {
+//   console.log("Height is not defined");
+// }
+
+// **********************************************
+
+//  Equality Operators: == vs. ===
+
+// const age = 18;
+// if (age === 18) console.log("You just becone an adult");
